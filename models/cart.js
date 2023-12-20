ba9119b0b7e0a96db5136f6d7c1f9ab0b744372a
Aygul Okdirova <aygul.okdirova@check24.de>
2023-12-20 15:10:33 +0100

    fix bug when cart is empty

--- models/cart.js
+++ models/cart.js
@@ -45,6 +45,9 @@ module.exports = class Cart {
       const productToDelete = updatedCart.products.find(
         (prod) => prod.id === id
       );
+      if (!productToDelete) {
+        return;
+      }
       const prodToDeleteQuantity = productToDelete.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
