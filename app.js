4974e4f792ad25349329bafd02bd896f627a6b38
Aygul Okdirova <aygul.okdirova@check24.de>
2024-01-09 11:48:23 +0100

    added user model

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorController = require("./controllers/error");
 
 // const sequelize = require("./util/database");
 // const Product = require("./models/product");
-// const User = require("./models/user");
+const User = require("./models/user");
 // const Cart = require("./models/cart");
 // const CartItem = require("./models/cart-item");
 // const Order = require("./models/order");
@@ -26,12 +26,12 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => console.log(err));
+  User.findById("659d23fc7fc1239908eb86cf")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
   next();
 });
 
